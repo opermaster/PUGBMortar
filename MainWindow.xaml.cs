755ac483e3be5672364350be2758fb567fd29be1
opermaster <fran4er@gmail.com>
2024-03-30 12:46:37 +0200

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace PUGBMortar
+{
+    /// <summary>
+    /// MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Btn_Calculate_Click(object sender, RoutedEventArgs e) {
+            int a, b, result;
+            try {
+                a = Convert.ToInt32(Text_A.Text);
+                b = Convert.ToInt32(Text_B.Text);
+                result = (int)Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+                Result.Text = $"Result= {result}";
+            }
+            catch {
+                Result.Text = $"Result= {0}";
+            }
+
+            
+        }
+    }
+}
